63091afae9568eb5722ffecb05be37bb2747c570
maheis <manfred@heister.email>
2023-10-18 08:39:54 +0200

    Aufgabenstellung: 18.10.2023 - Manche mögen's heiß

---
+++ 231018_Hottest/go.mod
@@ -0,0 +1,3 @@
+module github.com/maheis/CodingCircle/231018_Hottest
+
+go 1.21.2
---
+++ 231018_Hottest/main.go
@@ -0,0 +1,22 @@
+package main
+
+// 18.10.2023 - https://www.youtube.com/watch?v=0QPK9X1PtEE
+
+//Aufgabenstellung:
+//Gegeben sind eine Liste V und eine Ganzzahl k. Ermittle das laufende maximum für die Fenstergröße k.
+//
+//Beispiel:
+// V = [ 27, 9, 17, 2, 12, 8 ]
+// k = 3
+// => [ 27, 17, 17, 12 ]
+//
+//Bedingungen:
+// Die Laufzeit soll O(n) betragen, der Speiocherbedarf soll O(k) betragen.
+//
+//Lösung:
+
+//
+
+func main() {
+
+}
