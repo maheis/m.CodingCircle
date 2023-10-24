21210e91f142aaca21c8545727d6b25e0a4b57af
maheis <manfred@heister.email>
2023-10-24 20:44:23 +0200

    Dokumentation erweitert und Source vereinfacht

--- 231011_Median/heap/heap.go
+++ 231011_Median/heap/heap.go
@@ -14,58 +14,66 @@ import (
 )
 
 type MaxHeap []int
+
+func NewMaxHeap() MaxHeap {
+	h := MaxHeap{}
+	heap.Init(&h)
+	return h
+}
+func (h MaxHeap) Len() int           { return len(h) }            // Länge des Heaps
+func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }       // Vergleichsfunktion von zwei Elementen; Elemente wander nach oben im MaxHeap
+func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }  // Tausche zwei Elemente; spezielle Schreibweise in Go: h[i] wird zu h[j] und h[j] zu h[i], ohne dass eine temporäre Variable benötigt wird
+func (h *MaxHeap) Push(x any)        { *h = append(*h, x.(int)) } // Neues Element hinzufügen; spezielle Schreibweise in Go: x.(int) ist ein Typecast
+func (h *MaxHeap) Pop() any {
+	old := *h         // Kopie des Heaps bzw. Pointer in eine Variable überführen (dereferenzieren)
+	n := len(old) - 1 // Index auf das letzte Element
+	*h = old[0:n]     // Alles bis auf das letzte Element als Rückgabe in den (ByRef) Heap zurückschreiben
+	return old[n]     // Letztes Element zurückgeben
+}
+
 type MinHeap []int
 
-func (h MaxHeap) Len() int           { return len(h) }
-func (h MaxHeap) Less(i, j int) bool { return h[i] > h[j] }
-func (h MaxHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *MaxHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
+func NewMinHeap() MinHeap {
+	h := MinHeap{}
+	heap.Init(&h)
+	return h
 }
-func (h *MaxHeap) Pop() interface{} {
-	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
-}
-
 func (h MinHeap) Len() int           { return len(h) }
-func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h MinHeap) Less(i, j int) bool { return h[i] < h[j] } // Unterschied zum MaxHeap! Elemente wander nach unten im MinHeap
 func (h MinHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
-func (h *MinHeap) Push(x interface{}) {
-	*h = append(*h, x.(int))
-}
-func (h *MinHeap) Pop() interface{} {
+func (h *MinHeap) Push(x any)        { *h = append(*h, x.(int)) }
+func (h *MinHeap) Pop() any {
 	old := *h
-	n := len(old)
-	x := old[n-1]
-	*h = old[0 : n-1]
-	return x
+	n := len(old) - 1
+	*h = old[0:n]
+	return old[n]
 }
 
 func ContinuousMedian(values []int) []float64 {
 	// Liste der Medians
 	medians := make([]float64, 0, len(values))
 
-	var maxHeap MaxHeap
-	var minHeap MinHeap
-	heap.Init(&maxHeap)
-	heap.Init(&minHeap)
+	maxHeap := NewMaxHeap() // linke Hälfte, sortiert absteigend (höchstes Element ist das erste Element)
+	minHeap := NewMinHeap() // rechte Hölfte, sortiert aufsteigend (niedrigstes Element ist das erste Element)
 
 	for _, num := range values {
+		// Value in den Heap packen, der weniger Elemente hat
+		// Wenn der maxHeap noch leer ist oder die Zahl kleiner als das größte Element im maxHeap ist
 		if maxHeap.Len() == 0 || num <= maxHeap[0] {
+			// Packe es auf den MaxHeap
 			heap.Push(&maxHeap, num)
 		} else {
 			heap.Push(&minHeap, num)
 		}
 
+		// Balanciere die Heaps aus
 		if maxHeap.Len()-minHeap.Len() > 1 {
 			heap.Push(&minHeap, heap.Pop(&maxHeap))
 		} else if minHeap.Len()-maxHeap.Len() > 1 {
 			heap.Push(&maxHeap, heap.Pop(&minHeap))
 		}
 
+		// Ermittel den Median
 		if maxHeap.Len() == minHeap.Len() {
 			medians = append(medians, float64(maxHeap[0]+minHeap[0])/2.0)
 		} else if maxHeap.Len() > minHeap.Len() {
