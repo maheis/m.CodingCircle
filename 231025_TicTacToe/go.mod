8481481e599d91e50f3127da6ad3cd092f636acc
maheis <manfred@heister.email>
2023-10-25 12:11:18 +0200

    Basis für neue Aufgabe vom 25.10.2023 TicTacToe

---
+++ 231025_TicTacToe/go.mod
@@ -0,0 +1,3 @@
+module github.com/maheis/CodingCircle/231025_TicTacToe
+
+go 1.21.2
---
+++ 231025_TicTacToe/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 25.10.2023 - https://www.youtube.com/watch?v=ofs5v4jXa-k
+
+// Aufgabenstellung:
+// Gegeben seien die Spielfeldlänge n und eine Liste von Koordinaten.
+// Gesucht ist eine Funktion, die für ein Spielfeld der Größe n*n ent-
+// scheidet, ob die Koordinaten eine Gewinnposition enthalten.
+
+// Beispiel:
+// n = 3
+// [
+// 	(0,0), (0,2), (1,1), (2,2)
+// ] = true
+
+// Bedingungen:
+// Laufteit O(n²), Speicher O(n), Single-Pass
+
+func main() {
+	fmt.Println("OK")
+}
